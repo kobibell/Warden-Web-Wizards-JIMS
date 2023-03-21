74e378408ee418f500593ceed635abe51adaaabe
DerekMcCauley2002 <derek.mccauley2002@gmail.com>
2023-03-21 16:03:18 -0700

    Added default page functionality

--- src/jims_project/jims_app/urls.py
+++ src/jims_project/jims_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView
+from django.views.generic.base import TemplateView
 
 # Add your URL paths that belong to the jims_app here
 urlpatterns = [
+        path(r'', TemplateView.as_view(template_name='home_page.html')),
         path('login/', LoginView.as_view(template_name='login.html'), name='login'),
         path('home_page/', views.home_page, name='home_page'),
         path('accounts/profile/', views.home_page, name='profile'),
