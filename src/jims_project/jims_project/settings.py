7f978161c86cd90691368ec3889d902f25fee311
Alex Makhudyanstev <amakhmudy@Alexs-Laptop.local>
2023-04-12 14:06:14 -0700

    intialized dev database

--- src/jims_project/jims_project/settings.py
+++ src/jims_project/jims_project/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'jims_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'alex_testing',
+        'NAME': 'main_dev',
         'USER': 'webwardens',
         'PASSWORD': 'TakBXzeiVL2Ym8K',
         'HOST': 'jims.postgres.database.azure.com',
