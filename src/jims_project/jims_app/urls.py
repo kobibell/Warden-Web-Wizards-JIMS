b1b5d249794ddcbfd9df8260a6188f916ca34367
sheetalrprasad <sheetalraniprasad@gmail.com>
2023-04-13 00:22:20 -0700

    Basic transaction page setup

--- src/jims_project/jims_app/urls.py
+++ src/jims_project/jims_app/urls.py
@@ -7,6 +7,8 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
         path(r'', TemplateView.as_view(template_name='home_page.html')),
         path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+        # Need to redirect to home page link
+        path('', views.home_page, name='home_page'),
         path('home_page/', views.home_page, name='home_page'),
         path('accounts/profile/', views.home_page, name='profile'),
         path('accounts/home/', views.accounts_home, name='accounts_home'),
