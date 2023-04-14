3ef00430b571b3259330a0b7963166bedf3e912c
sheetalrprasad <sheetalraniprasad@gmail.com>
2023-04-13 22:23:32 -0700

    accounts page transactions

--- src/jims_project/jims_app/views.py
+++ src/jims_project/jims_app/views.py
@@ -1,13 +1,5 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login as auth_login
-from django.contrib import messages
-from django.middleware import csrf
-
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
 
 from .models import Accounts
 from .models import TransactionDetails
@@ -101,23 +93,19 @@ def accounts_transaction_details(request):
 def accounts_search_name(request):
     return render(request, 'accounts_search_name.html')
 
-@login_required
 def get_all_accounts(request):
     accounts = Accounts.objects.all()
     context = {'accounts': accounts}
     return render(request, 'account_list.html', context)
 
 def get_all_transaction_details(request):
+
     if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            email = form.cleaned_data.get('userEmail')
-            user = authenticate(username = username, password = raw_password, email = email)
-            login(request,user)
-            return redirect('home')
-    else:
-        form = SignUpForm()
-    return render(request, 'signup.html', {'form': form})
+        filter_by = request.POST.get('search_num')
+        if filter_by:
+            transaction_details = TransactionDetails.objects.filter(account_number=filter_by)
+            context = {'transaction_details': transaction_details}
+            return render(request, 'transaction_details_list.html', context)
+
+    return render(request, 'transaction_details_list.html')
+    
