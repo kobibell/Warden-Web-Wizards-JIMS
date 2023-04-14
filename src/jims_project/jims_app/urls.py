dcb7d8057747928fdf1194fb91a45961f9f2980d
sheetalrprasad <sheetalraniprasad@gmail.com>
2023-04-13 22:23:32 -0700

    accounts page transactions

--- src/jims_project/jims_app/urls.py
+++ src/jims_project/jims_app/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
         path('home_page/', views.home_page, name='home_page'),
         path('accounts/profile/', views.home_page, name='profile'),
         path('accounts/home/', views.accounts_home, name='accounts_home'),
-        path('accounts/transactions-details/', views.accounts_transaction_details, name='accounts_transaction_details'),
+        path('accounts/transactions-details-form/', views.accounts_transaction_details, name='accounts_transaction_details'),
+        path('accounts/transactions-details/', views.get_all_transaction_details, name='get_all_transaction_details'),
         path('accounts/search-name/', views.accounts_search_name, name='accounts_search_name'),
+        path('accounts/account_list/', views.get_all_accounts, name='get_all_accounts'),
 ]
