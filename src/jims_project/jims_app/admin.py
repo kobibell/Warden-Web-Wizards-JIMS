18d124882fd7e9135e0750c4abbe9f2cfaaf7114
alexmakh12 <alexandr.makhmudy@gmail.com>
2023-04-15 21:46:32 -0700

    functional user login and creation

--- src/jims_project/jims_app/admin.py
+++ src/jims_project/jims_app/admin.py
@@ -4,7 +4,7 @@ from .models import *
 # Register your models here.
 
 # Register the 'login' model with the Django admin site to enable admin management of the 'login' model data
-admin.site.register(Person)
 admin.site.register(Officer)
-
-
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    pass
