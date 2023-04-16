6f7eff14e51a528627917b73b81e669d02614cd5
alexmakh12 <alexandr.makhmudy@gmail.com>
2023-04-15 21:46:32 -0700

    functional user login and creation

--- src/jims_project/jims_app/urls.py
+++ src/jims_project/jims_app/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LoginView
 from django.views.generic.base import TemplateView
 
 # Add your URL paths that belong to the jims_app here
 urlpatterns = [
-        path(r'', TemplateView.as_view(template_name='home_page.html')),
-        path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+        # path('', TemplateView.as_view(template_name='home_page.html')),
+        path('', views.user_login, name='login'),
         # Need to redirect to home page link
-        #path('', views.home_page, name='home_page'),
+        # path('', views.home_page, name='home_page'),
         path('home_page/', views.home_page, name='home_page'),
         path('accounts/profile/', views.home_page, name='profile'),
         path('accounts/home/', views.accounts_home, name='accounts_home'),
--- src/jims_project/jims_app/views.py
+++ src/jims_project/jims_app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib import messages
+from django.middleware import csrf
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 from .models import Accounts
 from .models import TransactionDetails
