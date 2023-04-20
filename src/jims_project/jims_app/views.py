82187b1c55c0d41b87e2a9acbcf66a7492c98a80
sheetalrprasad <sheetalraniprasad@gmail.com>
2023-04-19 19:07:39 -0700

    transactions fixed

--- src/jims_project/jims_app/views.py
+++ src/jims_project/jims_app/views.py
@@ -143,7 +143,7 @@ def add_money(request):
 
             if(account):
                 account.balance = account.balance + form.cleaned_data['amount']
-                transaction = TransactionDetails.objects.create(account_number_id=account, transaction_type='D', transaction_amount=form.cleaned_data['amount'], transaction_date=timezone.now())
+                transaction = TransactionDetails.objects.create(account_number=account, transaction_type='D', transaction_amount=form.cleaned_data['amount'], transaction_date=timezone.now())
                 account.save()
                 transaction.save()
                 return render(request, 'add_money.html', {'message': 'Money added successfully'})
@@ -168,7 +168,7 @@ def withdraw_money(request):
                 if (account.balance < 0):
                     return render(request, 'withdraw_money.html', {'message': 'Insufficient funds'})
                 else:
-                    transaction = TransactionDetails.objects.create(account_number_id=account.account_number, transaction_type='W', transaction_amount=form.cleaned_data['amount'], transaction_date=timezone.now())
+                    transaction = TransactionDetails.objects.create(account_number=account, transaction_type='W', transaction_amount=form.cleaned_data['amount'], transaction_date=timezone.now())
                     account.save()
                     transaction.save()
                     return render(request, 'withdraw_money.html', {'message': 'Money withdrawn successfully'})
