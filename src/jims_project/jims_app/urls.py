72f3d1384e1b753009b47ec0cf9552e60a7c04dc
Alexandre Makhmudyantsev <alexandr.makhmudy@gmail.com>
2023-04-22 05:06:14 -0700

    removed inline css

--- src/jims_project/jims_app/urls.py
+++ src/jims_project/jims_app/urls.py
@@ -44,6 +44,7 @@ urlpatterns = [
         path('create-user/success/', views.create_user_success, name='create_user_success'),
 
         path('view-inmate/', views.view_inmate, name='view_inmate'),
+        path('add_inmate/', views.add_inmate, name='add_inmate'),
 
         path('view-inmate/inmate-details/', views.get_inmate_details, name='get_inmate_details'),
 
