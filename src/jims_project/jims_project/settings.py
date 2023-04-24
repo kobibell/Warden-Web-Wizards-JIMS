e33702a91465f1fbb05cb0bab2e2382a429b10dd
Alexandre Makhmudyantsev <alexandr.makhmudy@gmail.com>
2023-04-24 14:45:40 -0700

    changed db

--- src/jims_project/jims_project/settings.py
+++ src/jims_project/jims_project/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'jims_project.wsgi.application'
 DATABASES = {
      'default': {
          'ENGINE': 'django.db.backends.postgresql_psycopg2',
-         'NAME': 'ughh',
+         'NAME': 'dev',
          'USER': 'webwardens',
          'PASSWORD': 'TakBXzeiVL2Ym8K',
          'HOST': 'jims.postgres.database.azure.com',
