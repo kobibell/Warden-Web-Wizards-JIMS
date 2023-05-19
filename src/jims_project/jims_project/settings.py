0b227fefe14da8e8f30e0e6e71471ee6a9a241c6
Alexandre Makhmudyantsev <alexandr.makhmudy@gmail.com>
2023-05-18 20:11:16 -0700

    added local db

--- src/jims_project/jims_project/settings.py
+++ src/jims_project/jims_project/settings.py
@@ -82,15 +82,10 @@ WSGI_APPLICATION = 'jims_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-         'NAME': 'dev',
-         'USER': 'webwardens',
-         'PASSWORD': 'TakBXzeiVL2Ym8K',
-         'HOST': 'jims.postgres.database.azure.com',
-         'PORT': '5432',
-     }
-    
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # LOGGING = {
